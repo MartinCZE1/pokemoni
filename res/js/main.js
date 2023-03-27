fc0a68958948aa6667291fa95d7be0c587d4e0ef
Martin <martinhercik16@gmail.com>
2023-03-27 17:52:06 +0200

    První boj

--- res/js/main.js
+++ res/js/main.js
@@ -1,9 +1,23 @@
 const tutorialButton = document.getElementById("tutorialButton");
 const playButton = document.getElementById("playButton");
 
-const firstLocButton = document.getElementById("firstLocButton");
-const secondLocButton = document.getElementById("secondLocButton");
-const thirdLocButton = document.getElementById("thirdLocButton");
+const firstLocMenuButton = document.getElementById("firstLocMenuButton");
+const secondLocMenuButton = document.getElementById("secondLocMenuButton");
+const thirdLocMenuButton = document.getElementById("thirdLocMenuButton");
+
+const firstLocPlayerHPCounter = document.getElementById(
+  "firstLocPlayerHPCounter"
+);
+const firstLocEnemyHPCounter = document.getElementById(
+  "firstLocEnemyHPCounter"
+);
+
+const firstLocAttackOne = document.getElementById("firstLocAttackOne");
+const firstLocAttackTwo = document.getElementById("firstLocAttackTwo");
+const firstLocAttackThree = document.getElementById("firstLocAttackThree");
+
+let firstLocPlayerHP = 100;
+let firstLocEnemyHP = 100;
 
 tutorialButton.onclick = () => {
   start.style.display = "none";
@@ -17,26 +31,80 @@ playButton.onclick = () => {
   mainLoc.style.display = "block";
 };
 
-firstLocButton.onmouseenter = () => {
+firstLocMenuButton.onmouseenter = () => {
   arenaOnePreview.style.display = "block";
 };
 
-firstLocButton.onmouseleave = () => {
+firstLocMenuButton.onmouseleave = () => {
   arenaOnePreview.style.display = "none";
 };
 
-secondLocButton.onmouseenter = () => {
+secondLocMenuButton.onmouseenter = () => {
   arenaTwoPreview.style.display = "block";
 };
 
-secondLocButton.onmouseleave = () => {
+secondLocMenuButton.onmouseleave = () => {
   arenaTwoPreview.style.display = "none";
 };
 
-thirdLocButton.onmouseenter = () => {
+thirdLocMenuButton.onmouseenter = () => {
   arenaThreePreview.style.display = "block";
 };
 
-thirdLocButton.onmouseleave = () => {
+thirdLocMenuButton.onmouseleave = () => {
   arenaThreePreview.style.display = "none";
 };
+
+firstLocMenuButton.onclick = () => {
+  mainLoc.style.display = "none";
+  firstLoc.style.display = "block";
+      
+  firstLocFight();
+};
+
+function firstLocFight() {
+  firstLocFightInterval = setInterval(() => {
+    firstLocPlayerHP -= 5;
+    firstLocPlayerHPCounter.innerHTML = `Počet životů hráče: ${firstLocPlayerHP}`;
+    if (firstLocPlayerHP <= 0) {
+      clearInterval(firstLocFightInterval);
+      firstLocEnemyWin.style.display = "block";
+      endGame = setInterval(() => {
+        firstLoc.style.display = "none";
+        mainLoc.style.display = "block";
+        firstLocEnemyWin.style.display = "none";
+        firstLocPlayerHP = 100;
+        firstLocEnemyHP = 100;
+        clearInterval(endGame);
+      }, 1000);
+    }
+  }, 5000);
+  firstLocAttackOne.onclick = () => {
+    firstLocEnemyHP -= 5;
+    firstLocEnemyHPCounter.innerHTML = `Počet životů nepřítele ${firstLocEnemyHP}`;
+    firstLocEnemyDead();
+  };
+  firstLocAttackTwo.onclick = () => {
+    firstLocEnemyHP -= 15;
+    firstLocEnemyHPCounter.innerHTML = `Počet životů nepřítele ${firstLocEnemyHP}`;
+    firstLocEnemyDead();
+  };
+  firstLocAttackThree.onclick = () => {
+    firstLocEnemyHP -= 25;
+    firstLocEnemyHPCounter.innerHTML = `Počet životů nepřítele ${firstLocEnemyHP}`;
+    firstLocEnemyDead();
+  };
+}
+function firstLocEnemyDead() {
+  if (firstLocEnemyHP <= 0) {
+    clearInterval(firstLocFightInterval);
+    firstLocPlayerWin.style.display = "block";
+    endGame = setInterval(() => {
+      firstLoc.style.display = "none";
+      mainLoc.style.display = "block";
+      firstLocPlayerWin.style.display = "none";
+      firstLocPlayerHP = 100;
+      firstLocEnemyHP = 100;clearInterval(endGame);
+    }, 1000);
+  }
+}
